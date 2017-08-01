bb3a7a56ff0aecf9f045069869aac840a99885e1
Dmytro <ddorotiuk@gmail.com>
2017-08-01 11:08:12 +0200

    Add html structure

---
+++ js/App.js
@@ -0,0 +1,33 @@
+// COMUNICATION WITH SERVER
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2079',
+  'X-Auth-Token': '6c4c2a1456237b0a0573dfb1200fc780'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
